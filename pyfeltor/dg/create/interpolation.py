a4b2808be3cd42b99d8f1d61ced7c67eb888141c
Matthias <mattwi@fysik.dtu.dk>
2023-02-15 11:48:03 +0100

    Fixed interpolation tests
    turns out the tests were wrong, not the implementation

--- pyfeltor/dg/create/interpolation.py
+++ pyfeltor/dg/create/interpolation.py
@@ -10,26 +10,28 @@ def shift(grid, x, bcs):
         (used internally by interpolation)
     """
     negative = False
-    for i in range( 0, grid.ndim):
-        if( bcs[i] == bc.PER):
-            N = np.floor((x[i]-grid.x0[i])/(grid.x1[i]-grid.x0[i])) # ... -2[ -1[ 0[ 1[ 2[ ...
-            x[i] = x[i] - N*(grid.x1[i]-grid.x0[i]) # shift
+    for i in range(0, grid.ndim):
+        if bcs[i] == bc.PER:
+            N = np.floor(
+                (x[i] - grid.x0[i]) / (grid.x1[i] - grid.x0[i])
+            )  # ... -2[ -1[ 0[ 1[ 2[ ...
+            x[i] = x[i] - N * (grid.x1[i] - grid.x0[i])  # shift
         # mirror along boundary as often as necessary
-        while( (x[i]<grid.x0[i]) or (x[i]>grid.x1[i]) ):
-            if( x[i] < grid.x0[i]):
-                x[i] = 2.*grid.x0[i] - x[i];
+        while (x[i] < grid.x0[i]) or (x[i] > grid.x1[i]):
+            if x[i] < grid.x0[i]:
+                x[i] = 2.0 * grid.x0[i] - x[i]
                 # every mirror swaps the sign if Dirichlet
-                if( (bcs[i] == bc.DIR) or (bcs[i] == bc.DIR_NEU)):
-                    negative = not negative # swap sign
-            if( x[i] > grid.x1[i]):
-                x[i] = 2.*grid.x1[i] - x[i]
-                if( (bcs[i] == bc.DIR) or (bcs[i] == bc.NEU_DIR)):
+                if (bcs[i] == bc.DIR) or (bcs[i] == bc.DIR_NEU):
+                    negative = not negative  # swap sign
+            if x[i] > grid.x1[i]:
+                x[i] = 2.0 * grid.x1[i] - x[i]
+                if (bcs[i] == bc.DIR) or (bcs[i] == bc.NEU_DIR):
                     # notice the different boundary NEU_DIR to the above DIR_NEU !
-                    negative = not negative # swap sign
+                    negative = not negative  # swap sign
     return negative, x
 
 
-def interpolation( xs, grid, bcs):
+def interpolation(xs, grid, bcs):
     """ interpolation matrix that evaluate points on given grid
 
         order is ... z y x
@@ -38,43 +40,49 @@ def interpolation( xs, grid, bcs):
     rows = []
     cols = []
     vals = []
-    forward = [ops.forward( grid.n[dim]) for dim in range(0,grid.ndim)]
+    forward = [ops.forward(grid.n[dim]) for dim in range(0, grid.ndim)]
 
-    for i in range(0, len( xs[0])):
-        X = np.array([xs[k][i] for k in range(0,grid.ndim)])
-        negative, X = shift( grid, X, bcs)
-        xnn = (X - grid.x0)/grid.h()
-        nn = np.floor( xnn)
-        xn = 2.0*xnn - (2*nn+1)
-        for k in range( 0, grid.ndim):
+    for i in range(0, len(xs[0])):
+        X = np.array([xs[k][i] for k in range(0, grid.ndim)])
+        negative, X = shift(grid, X, bcs)
+        xnn = (X - grid.x0) / grid.h()
+        nn = np.floor(xnn)
+        xn = 2.0 * xnn - (2 * nn + 1)
+        for k in range(0, grid.ndim):
             if nn[k] == grid.N[k]:
                 nn[k] -= 1
                 xn[k] = 1
 
         # evaluate Legendre polynomials at (xn)...
-        px = [(np.polynomial.legendre.legvander( xn[k], grid.n[k]-1) @ forward[k]).ravel() for k in range( 0, grid.ndim)]
+        px = [
+            (
+                np.polynomial.legendre.legvander(xn[k], grid.n[k] - 1) @ forward[k]
+            ).ravel()
+            for k in range(0, grid.ndim)
+        ]
 
         # generate the list of index permutations
-        tuples = range(0,grid.n[0])
+        tuples = []
         if grid.ndim == 1:
-            tuples = [(a,) for a in tuples]
-        for k in range( 1, grid.ndim):
-            tuples = itertools.product( tuples, range( 0, grid.n[k]))
-            if k > 1:
-                tuples = [(a,b,c) for (a,b),c in tuples]
-            
+            tuples = [(a,) for a in range(0, grid.n[0])]
+        elif grid.ndim == 2:
+            tuples = itertools.product(range(0, grid.n[0]), range(0, grid.n[1]))
+        elif grid.ndim == 3:
+            tuples = itertools.product(range(0, grid.n[0]), range(0, grid.n[1]), range(0, grid.n[2]))
+        else:
+            raise Exception( "interpolation not implemented for ndim > 3")
         for it in tuples:
-            I = 0 # the index to push back
-            V = 1 # the value to push back
-            for kk in range( 0, grid.ndim):
-                I = ((I*grid.N[kk] + nn[kk])*grid.n[kk] + it[kk])
-                V = V*px[kk][it[kk]]
-            #print( i, I, V)
+            I = 0  # the index to push back
+            V = 1  # the value to push back
+            for kk in range(0, grid.ndim):
+                I = (I * grid.N[kk] + nn[kk]) * grid.n[kk] + it[kk]
+                V = V * px[kk][it[kk]]
+            # print( i, I, V)
             rows.append(i)
             cols.append(round(I))
-            if not negative :
-                vals.append( V)
-            else :
+            if not negative:
+                vals.append(V)
+            else:
                 vals.append(-V)
     # sort
     rows, cols, vals = zip(*sorted(zip(rows, cols, vals)))
--- tests/test_interpolation.py
+++ tests/test_interpolation.py
@@ -1,59 +1,71 @@
 from pyfeltor import dg
 import numpy as np
 
-n, Nx, Ny, Nz = 3,9,5,4
-def test_interpolation1d():
-    print( "1D INTERPOLATION TEST")
-    passed = True
-    g1d = dg.Grid( [-np.pi], [0], [n], [Nx])
-    field = dg.evaluate( lambda x : x, g1d)
-    x = [(i+0.5)*g1d.h()[0]/g1d.n[0] for i in range(0, g1d.size())]
 
-    interp = dg.create.interpolation( [x], g1d, [dg.bc.DIR])
-    x_inter = interp.dot( field)
-    for i in range( g1d.size()):
-        if( x[i] - x_inter[i] > 1e-14):
+def test_interpolation1d():
+    print("1D INTERPOLATION TEST")
+    passed = True
+    n, Nx = 3, 9
+    g1d = dg.Grid([-np.pi], [0], [n], [Nx])
+    field = dg.evaluate(lambda x: x, g1d)
+    x = [(i + 0.5) * g1d.h()[0] / g1d.n[0] for i in range(0, g1d.size())]
+
+    interp = dg.create.interpolation([x], g1d, [dg.bc.DIR])
+    x_inter = interp.dot(field)
+    for i in range(g1d.size()):
+        if x[i] - x_inter[i] > 1e-14:
             print(f"X NOT EQUAL {i}\t{x[i]}  \t{x_inter[i]}")
             passed = False
-    assert( passed)
-    
+    assert passed
+
 
 def test_interpolation2d():
-    print( "2D INTERPOLATION TEST")
+    print("2D INTERPOLATION TEST")
 
-    g = dg.Grid( [-5, -10], [5, 10], [n,n], [Ny, Nx]);
-    equi = dg.Grid( [-5, -10], [5, 10], [1,1], [n*Ny, n*Nx]);
-    y = dg.evaluate( lambda y,x : y , equi)
-    x = dg.evaluate( lambda y,x : x , equi)
+    n, Nx, Ny = 3, 32, 32
+    g = dg.Grid(x0=[-5 * np.pi, -np.pi], x1=[-4 * np.pi, 0], n=[n, n], N=[Ny, Nx])
+    equi = dg.Grid(
+        x0=[-5 * np.pi, -np.pi], x1=[-4 * np.pi, 0], n=[1, 1], N=[n * Ny, n * Nx]
+    )
+    y = dg.evaluate(lambda y, x: y, equi)
+    x = dg.evaluate(lambda y, x: x, equi)
 
-    print ( "Length of arrays", len(y), len(x))
-    interp = dg.create.interpolation( [y,x], g, [dg.bc.DIR, dg.bc.PER])
-    vec = dg.evaluate( lambda y,x: np.sin(x)*np.sin(y), g)
-    inter = interp.dot( vec)
-    interE = dg.evaluate( lambda y,x: np.sin(x)*np.sin(y), equi)
-    print( inter[0:10])
-    print( interE[0:10])
-    error = np.sum ( (inter-interE)**2)/np.sum(inter**2)
+    interp = dg.create.interpolation([y, x], g, [dg.bc.DIR, dg.bc.DIR])
+    vec = dg.evaluate(lambda y, x: np.sin(x) * np.sin(y), g)
+    inter = interp.dot(vec)
+    interE = dg.evaluate(lambda y, x: np.sin(x) * np.sin(y), equi)
+    error = np.sum((inter - interE) ** 2) / np.sum(inter ** 2)
     print(f"Error is {np.sqrt(error)} (should be small)!")
-    assert np.isclose( np.sqrt(error), 0)
+    assert np.isclose(np.sqrt(error), 0, atol=1e-5)
 
 
 def test_interpolation3d():
-    print( "3D INTERPOLATION TEST")
+    print("3D INTERPOLATION TEST")
+    n, Nx, Ny, Nz = 3, 9, 5, 4
 
-    g = dg.Grid( [-7, -5, -10], [-3, 5, 10], [n,n,n], [Nz, Ny, Nx]);
-    equi = dg.Grid( [-7, -5, -10], [-3, 5, 10], [1,1,1], [n*Nz, n*Ny, n*Nx]);
-    z = dg.evaluate( lambda z,y,x : z , equi)
-    y = dg.evaluate( lambda z,y,x : y , equi)
-    x = dg.evaluate( lambda z,y,x : x , equi)
+    g = dg.Grid([-7, -5, -10], [-3, 5, 10], [1, n, n], [Nz, Ny, Nx])
+    equi = dg.Grid([-7, -5, -10], [-3, 5, 10], [1, 1, 1], [1 * Nz, n * Ny, n * Nx])
+    z = dg.evaluate(lambda z, y, x: z, equi)
+    y = dg.evaluate(lambda z, y, x: y, equi)
+    x = dg.evaluate(lambda z, y, x: x, equi)
 
-    print ( "Length of arrays", len(z), len(y), len(x))
-    interp = dg.create.interpolation( [z,y,x], g, [dg.bc.DIR, dg.bc.DIR, dg.bc.PER])
-    vec = dg.evaluate( lambda z,y,x: np.sin(x)*np.sin(y)*np.sin(z), g)
-    inter = interp.dot( vec)
-    interE = dg.evaluate( lambda z,y,x: np.sin(x)*np.sin(y)*np.sin(z), equi)
-    print( inter[0:10])
-    print( interE[0:10])
-    error = np.sum ( (inter-interE)**2)/np.sum(inter**2)
-    print(f"Error is {np.sqrt(error)} (should be small)!")
-    assert np.isclose( np.sqrt(error), 0)
+    interp = dg.create.interpolation([z, y, x], g, [dg.bc.DIR, dg.bc.DIR, dg.bc.PER])
+    vec = dg.evaluate(lambda z, y, x: np.sin(x) * np.sin(y) * np.sin(z), g)
+    inter = interp.dot(vec)
+    # These values are from interpolation_t.cu
+    solution = np.array(
+        [
+            -0.0388107,
+            0.108781,
+            0.200279,
+            0.188096,
+            0.07971,
+            -0.0697783,
+            -0.189282,
+            -0.20544,
+            -0.115663,
+            0.0414357,
+        ]
+    )
+    print("Pointwise error ", inter[0:10] - solution)
+    assert np.allclose(inter[0:10], solution)
